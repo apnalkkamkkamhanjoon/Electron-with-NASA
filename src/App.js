6bf7f451d6cb2d1f1db68525ea3cd17b4873e8bf
한준 <s22073@gsm.hs.kr>
2023-03-13 18:11:52 +0900

    NASA

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+const API_KEY = "3w5zpFtCojyBFbMaSODzR4UNf9wYhUI6OTWFFohI";
+const URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
 function App() {
+  const [todayimg, setImgcover] = useState();
+  const [date, setDate] = useState();
+  useEffect(() => {
+    axios.get(URL).then((res) => {
+      console.log(res.data);
+      setImgcover(res.data.url);
+      setDate(res.data.date);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="todayH1">Astronomy Picture of the Day</h1>
+      <img alt="todayimg" src={todayimg} className="todayImg"/>
+      <p className="today">{date}</p>
+    </>
   );
 }
 
